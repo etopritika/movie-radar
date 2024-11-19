1b34403cde8c07784026118149d663e4c8a0a723
etopritika <dimaprytyka@gmail.com>
2024-11-19 18:55:30 +0200

    Updated search page

--- src/app/search/[...query]/page.tsx
+++ src/app/search/[...query]/page.tsx
@@ -1,3 +1,4 @@
+import MovieGallery from "@/components/movie-gallery";
 import { fetchMovies } from "@/lib/api";
 import { Movie } from "@/lib/types";
 
@@ -9,15 +10,12 @@ export default async function SearchPage({
   const query = (await params).query;
   const searchQuery = query[0] || "";
   const page = Number(query[1]) || 1;
-  const movies = await fetchMovies(searchQuery, page);
+  const movies = (await fetchMovies(searchQuery, page)) as Movie[];
 
   return (
-    <div>
-      <ul>
-        {movies.map((movie: Movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <h1 className="sr-only">Results for - {searchQuery}</h1>
+      <MovieGallery movies={movies} />
+    </section>
   );
 }
