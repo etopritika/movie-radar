bc3dbf1b166e58c203580d673638b101d7e5bb0e
etopritika <dimaprytyka@gmail.com>
2024-11-24 19:39:41 +0200

    Added library component and types

--- src/app/library/[pageNumber]/page.tsx
+++ src/app/library/[pageNumber]/page.tsx
@@ -1,20 +1,36 @@
-// import { Movie } from "@/lib/types";
+"use client";
 
-export default async function Library({
-  params,
-}: {
-  params: Promise<{ pageNumber: string[] }>;
-}) {
-  //   const { pageNumber } = await params;
-  //   const page = Number(pageNumber) || 1;
+import MovieGallery from "@/components/movie-gallery";
+import MoviePagination from "@/components/pagination";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { LocalStorageMovie } from "@/lib/types";
+
+export default function Library() {
+  const [movies, setMovies] = useState<LocalStorageMovie[]>([]);
+
+  const pathname = usePathname();
+  const splitedPath = pathname.split("/")[2];
+
+  const page = Number(splitedPath) || 1;
+  const itemsPerPage = 20;
+
+  useEffect(() => {
+    const savedMovies = JSON.parse(localStorage.getItem("movies") || "[]");
+    setMovies(savedMovies);
+  }, []);
+
+  const totalPages = Math.ceil(movies.length / itemsPerPage);
+  const displayedMovies = movies.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
+
   return (
-    <div>
-      <span>Library</span>
-      {/* <ul>
-        {movies.map((movie: Movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul> */}
-    </div>
+    <section>
+      <h1 className="sr-only">Movie Library</h1>
+      <MovieGallery movies={displayedMovies} />
+      <MoviePagination currentPage={page} totalPages={totalPages} />
+    </section>
   );
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,19 +1,24 @@
-export type Movie = {
+export interface IBaseMovie {
+  id: number;
+  title: string;
+  poster_path: string | null;
+  overview: string;
+  genre_ids: number[];
+  release_date: string;
+}
+
+export type Movie = IBaseMovie & {
   adult: boolean;
   backdrop_path: string | null;
-  genre_ids: number[];
-  id: number;
   original_language: string;
   original_title: string;
-  overview: string;
   popularity: number;
-  poster_path: string | null;
-  release_date: string;
-  title: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
 };
+
+export type LocalStorageMovie = IBaseMovie;
 
 export type MovieDetails = {
   adult: boolean;
