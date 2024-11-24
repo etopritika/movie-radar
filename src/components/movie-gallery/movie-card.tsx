625c142867c7b6c8c50d77e0c79713f82eabc83a
etopritika <dimaprytyka@gmail.com>
2024-11-24 12:37:40 +0200

    Updated styles in movie card

--- src/components/movie-gallery/movie-card.tsx
+++ src/components/movie-gallery/movie-card.tsx
@@ -44,7 +44,7 @@ export default function MovieCard({
   };
 
   return (
-    <li className="relative transition-transform duration-300 hover:-translate-y-1 hover:shadow-[4px_4px_18px_rgba(255,0,0,0.6)] rounded-[10px] sm:rounded-[15px] overflow-hidden">
+    <li className="relative transition-transform duration-300 hover:-translate-y-1 hover:shadow-[6px_6px_18px_rgba(255,0,0,0.6)] rounded-[10px] sm:rounded-[15px] overflow-hidden border-none">
       <button onClick={handleOpenModal} className="w-full h-full">
         <Image
           className="h-full w-full max-h-[402px] sm:max-h-[455px] md:max-h-[455px] xl:max-h-[574px]"
