e0610bd377db23644f387c8038c8de6f4d947f40
etopritika <dimaprytyka@gmail.com>
2024-11-24 19:40:21 +0200

    updated gallery and pagination

--- src/components/movie-gallery/index.tsx
+++ src/components/movie-gallery/index.tsx
@@ -1,8 +1,8 @@
-import { Movie } from "@/lib/types";
+import { IBaseMovie } from "@/lib/types";
 import MovieCard from "./movie-card";
 
 interface MovieGalleryProps {
-  movies: Movie[];
+  movies: IBaseMovie[];
 }
 
 export default function MovieGallery({ movies }: MovieGalleryProps) {
@@ -10,14 +10,7 @@ export default function MovieGallery({ movies }: MovieGalleryProps) {
     <div className="mb-10 sm:mb-[60px]">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-5 md:gap-8 xl:gap-x-4 xl:gap-y-8">
         {movies.map(
-          ({
-            id,
-            title,
-            poster_path,
-            overview,
-            release_date,
-            genre_ids,
-          }: Movie) => (
+          ({ id, title, poster_path, overview, release_date, genre_ids }) => (
             <MovieCard
               key={id}
               id={id}
