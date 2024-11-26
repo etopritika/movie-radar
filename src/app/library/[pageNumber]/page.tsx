f7bd0f3071e89a9de378902ecd199202aeea96d7
etopritika <dimaprytyka@gmail.com>
2024-11-26 13:17:24 +0200

    Updated library structure

--- src/app/library/[pageNumber]/page.tsx
+++ src/app/library/[pageNumber]/page.tsx
@@ -1,55 +1,18 @@
-"use client";
-
-import MoviePagination from "@/components/pagination";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { LocalStorageMovie } from "@/lib/types";
 import MovieGalleryClient from "@/components/movie-gallery/movie-gallery-client";
-import { GallerySkeleton } from "@/components/skeletons/gallery-skeleton";
-import { useLibraryStore } from "@/store/update-library";
 
-export default function Library() {
-  const updateTrigger = useLibraryStore((state) => state.updateTrigger);
-  const [movies, setMovies] = useState<LocalStorageMovie[]>([]);
-  const [loading, setLoading] = useState(true);
-  const pathname = usePathname();
-  const splitedPath = pathname.split("/")[2];
+export default async function Library({
+  params,
+}: {
+  params: Promise<{ pageNumber: string }>;
+}) {
+  const { pageNumber } = await params;
 
-  const page = Number(splitedPath) || 1;
-  const itemsPerPage = 20;
-
-  useEffect(() => {
-    setLoading(true);
-
-    try {
-      const savedMovies = JSON.parse(localStorage.getItem("movies") || "[]");
-
-      if (savedMovies.length > 0) {
-        const startIndex = (page - 1) * itemsPerPage;
-        const endIndex = page * itemsPerPage;
-        const paginatedMovies = savedMovies.slice(startIndex, endIndex);
-        setMovies(paginatedMovies);
-      } else {
-        setMovies([]);
-      }
-    } catch (error) {
-      console.error("Error reading from localStorage:", error);
-      setMovies([]);
-    }
-
-    setLoading(false);
-  }, [page, updateTrigger]);
-
-  const totalPages = Math.ceil(
-    (JSON.parse(localStorage.getItem("movies") || "[]").length || 0) /
-      itemsPerPage
-  );
+  const page = Number(pageNumber) || 1;
 
   return (
     <section>
       <h1 className="sr-only">Movie Library</h1>
-      {loading ? <GallerySkeleton /> : <MovieGalleryClient movies={movies} />}
-      <MoviePagination currentPage={page} totalPages={totalPages} />
+      <MovieGalleryClient page={page} />
     </section>
   );
 }
--- src/components/movie-gallery/movie-gallery-client.tsx
+++ src/components/movie-gallery/movie-gallery-client.tsx
@@ -1,32 +1,56 @@
 "use client";
 
-import { IBaseMovie } from "@/lib/types";
+import { LocalStorageMovie } from "@/lib/types";
 import MovieCard from "./movie-card";
+import { useLibraryStore } from "@/store/update-library";
+import { useEffect, useState } from "react";
+import MoviePagination from "../pagination";
 
 interface MovieGalleryClientProps {
-  movies: IBaseMovie[];
+  page: number;
 }
 
-export default function MovieGalleryClient({
-  movies,
-}: MovieGalleryClientProps) {
+export default function MovieGalleryClient({ page }: MovieGalleryClientProps) {
+  const updateTrigger = useLibraryStore((state) => state.updateTrigger);
+  const [movies, setMovies] = useState<LocalStorageMovie[]>([]);
+  const [totalMovies, setTotalMovies] = useState(0);
+
+  const itemsPerPage = 20;
+
+  useEffect(() => {
+    const savedMovies = JSON.parse(localStorage.getItem("movies") || "[]");
+
+    setTotalMovies(savedMovies.length);
+
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = page * itemsPerPage;
+    const paginatedMovies = savedMovies.slice(startIndex, endIndex);
+
+    setMovies(paginatedMovies);
+  }, [page, updateTrigger]);
+
+  const totalPages = Math.ceil(totalMovies / itemsPerPage);
+
   return (
-    <div className="mb-10 sm:mb-[60px]">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-5 md:gap-8 xl:gap-x-4 xl:gap-y-8">
-        {movies.map(
-          ({ id, title, poster_path, overview, release_date, genre_ids }) => (
-            <MovieCard
-              key={id}
-              id={id}
-              title={title}
-              poster_path={poster_path}
-              overview={overview}
-              release_date={release_date}
-              genre_ids={genre_ids}
-            />
-          )
-        )}
-      </ul>
-    </div>
+    <>
+      <div className="mb-10 sm:mb-[60px]">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-5 md:gap-8 xl:gap-x-4 xl:gap-y-8">
+          {movies.map(
+            ({ id, title, poster_path, overview, release_date, genre_ids }) => (
+              <MovieCard
+                key={id}
+                id={id}
+                title={title}
+                poster_path={poster_path}
+                overview={overview}
+                release_date={release_date}
+                genre_ids={genre_ids}
+              />
+            )
+          )}
+        </ul>
+      </div>
+      <MoviePagination currentPage={page} totalPages={totalPages} />
+    </>
   );
 }
