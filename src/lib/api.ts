9d23858c90bc1a3a603b449d7b2853028ab6e578
etopritika <dimaprytyka@gmail.com>
2024-11-26 23:24:57 +0200

    fixed api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ export async function fetchTrending(page: number = 1) {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-  const url = `${BASE_URL}/trendin/movie/week?api_key=${API_KEY}&page=${page}&language=en-US`;
+  const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${page}&language=en-US`;
 
   try {
     const response = await fetch(url);
