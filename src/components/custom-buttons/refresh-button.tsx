4c2c06ef408e82f66bef1a9fd2448cfa16727677
etopritika <dimaprytyka@gmail.com>
2024-11-28 14:27:53 +0200

    Refactor refresh button

--- src/components/custom-buttons/refresh-button.tsx
+++ src/components/custom-buttons/refresh-button.tsx
@@ -9,6 +9,7 @@ export default function RefreshButton() {
   return (
     <Button
       onClick={router.refresh}
+      aria-label="Refresh the page"
       className="rounded-full border-none bg-red-700 px-8 py-[14px] text-xs font-medium uppercase leading-[14px] text-white transition-colors hover:bg-red-400 sm:text-sm md:px-10 md:py-6 md:text-base"
     >
       Refresh page <RotateCcw />
